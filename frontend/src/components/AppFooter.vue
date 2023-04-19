0d4b7c71046dcd2c975618293cab0be6322786d7
Szabolcs Hegyi <hegyi.szabolcs@students.jedlik.eu>
2023-04-19 13:49:55 +0200

    footer

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,8 +1,32 @@
 <template>
-    <div class="p-3 mb-2 bg-success text-white">
-        <p class="py-2 footer bg-green">&copy; Lemezbázis</p>
-        <p>Készítők: Dörnyei Laura, Hegyi Szabolcs, Szombathelyi Levente</p>
-    </div>
+    <footer class="bg-dark text-center text-white footer">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Készítette: Dörnyei Laura, Szombathelyi Levente, Hegyi Szabolcs
+    
+  </div>
+  <!-- Copyright -->
+</footer>
  </template>
  
  <script setup>
