4dd39e24b20aa7b8b78ace616c407a5928b45180
laladornyei <laladornyei@gmail.com>
2023-04-19 12:20:43 +0200

    kép működik

--- frontend/src/components/TermekCard.vue
+++ frontend/src/components/TermekCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card mb-3 shadow">
-        <img class="card-img-top" :src="`http://localhost:3000/api/public/uploads/${lemezData.photo}`" alt="Card image cap">
+        <img class="card-img-top" :src="`http://localhost:3000/${lemezData.photo}`" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title"><router-link @click="setId(lemezData.id)" class="nav-link" :to="`/eladas/${lemezData.lemezcim}`">{{ lemezData.lemezcim }}</router-link></h5>
             <p class="card-text"><b>{{lemezData.eloado}}</b> - {{lemezData.evjarat}}</p>
@@ -10,7 +10,7 @@
             <!-- <div v-for="szam in lemezData.zeneszamok">
                 <p>{{ szam }}</p>
             </div> -->
-            <!-- <p class="card-text">{{lemezData.user}}</p>  -->
+            <p class="card-text">{{lemezData.photo}}</p> 
             <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
         </div>
     </div>
