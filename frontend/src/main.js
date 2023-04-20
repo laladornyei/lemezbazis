74402d9c6b7bbd7de742b8cdf14cabba3727d45f
Szabolcs Hegyi <hegyi.szabolcs@students.jedlik.eu>
2023-04-20 10:06:24 +0200

    jobb

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,17 +6,10 @@ import { createPinia } from "pinia";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './assets/css/style.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
-const app = createApp(App);
-app.use(createPinia());
-app.use(router);
-app.mount("#app");
-library.add(faUserSecret)
+const app = createApp(App)
+ app.use(createPinia());
+ app.use(router);
+ app.mount("#app");
 
-createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon)
-.mount('#app')
 
