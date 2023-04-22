4d9f6f5e2ec1697384fc071916abf198113e1991
Szabolcs Hegyi <hegyi.szabolcs@students.jedlik.eu>
2023-04-22 19:01:20 +0200

    sda

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,7 +4,7 @@
     <p>A bakelit lemezek adás-vételére Magyarországon még nem volt példa, ezért oldalunk megfelelő Önnek, ha szeretné bővíteni, vagy éppen megválni a nem szükséges daraboktól.  </p>
     <p class="border-bottom ">A bakelit lemez egy hagyományos analóg hanghordozó, amelyet a 20. század elején fejlesztettek ki, és a zenei iparban használtak a 20. század közepéig. A lemezek egyedülállóak abban a tekintetben, hogy az analóg technológia miatt minden másolat és felvétel valóban egyedi, ez azt jelenti, hogy minden egyes lemez más hangminőséget adhat vissza.</p>
 
-    <h3>Jelenleg felkapottak:</h3>
+    <h3>Jelenleg felkapottak!:</h3>
     
 </template>
 
