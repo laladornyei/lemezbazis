55835e0dfa8c32db08042a7da45c6bd537f99505
laladornyei <laladornyei@gmail.com>
2023-04-23 18:44:22 +0200

    bejelentkezés fix

--- frontend/src/views/BejelentkezesView.vue
+++ frontend/src/views/BejelentkezesView.vue
@@ -17,7 +17,7 @@
                     <input type="password" class="form-control" id="password" v-model="password">
                 </div>
             </div>
-            <button type="submit" class="btn btn-success m-1" @click="login(),getBejelentkezett()">Bejelentkezés</button>
+            <button type="submit" class="btn btn-success m-1" @click="login(),$router.push(`/`)">Bejelentkezés</button>
             <div class="m-3">
 
                 <router-link to="/elfelejtett-jelszo">Elfelejtetted a jelszavad?</router-link>
