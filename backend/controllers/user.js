04f2406eea250f7b0a9515108a4a4c8539bd519d
Szabolcs Hegyi <hegyi.szabolcs@students.jedlik.eu>
2023-04-28 19:52:35 +0200

    pdf, kerdezesview footerfix

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,12 +5,14 @@ const User= require('../models/User');
 // @access Public
 exports.getUserById = async (req, res, next) => {
     try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-    return res.status(404).json({ success: false, msg: 'User not found' });
-    }
-    res.status(200).json({ success: true, data: user });
+        const user = await User.findById(req.params.id).populate('termekek');
+  
+      if (!user) {
+        return res.status(404).json({ success: false, msg: 'User not found' });
+      }
+  
+      res.status(200).json({ success: true, data: user });
     } catch (error) {
-    next(error);
+      next(error);
     }
-    };
+  };
